866f04ecc5c04caeb87a2adb55439e9e5534b8de
ochudnovskiy <maxxwell131@yahoo.com>
2017-01-29 19:33:12 +0200

    Calculate with add operation ^

--- Calculate.java
+++ Calculate.java
@@ -1,21 +1,36 @@
 public class Calculate{
 	public static void main(String[] args) {
-			System.out.println("Calculate ...");
+        System.out.println("Calculate ...");
 
-			int first = Integer.valueOf(args[0]);
-			int second = Integer.valueOf(args[1]);
-			int result = 0;
+        double first = Double.valueOf(args[0]);
+        double second = Double.valueOf(args[1]);
+        double result = 0;
 
-			result = first + second;
-			System.out.println(first + " + " + second + " = " + result);
+        result = first + second;
+        System.out.println(first + " + " + second + " = " + result);
 
-			result = first - second;
-			System.out.println(first + " - " + second + " = " + result);
+        result = first - second;
+        System.out.println(first + " - " + second + " = " + result);
 
-			result = first * second;
-			System.out.println(first + " * " + second + " = " + result);
+        result = first * second;
+        System.out.println(first + " * " + second + " = " + result);
 
-			result = first / second;
-			System.out.println(first + " / " + second + " = " + result);
-	}
+        result = first / second;
+        System.out.println(first + " / " + second + " = " + result);
+
+        result = power(first,second);
+        System.out.println(first + " ^ " + second + " = " + result);
+    }
+
+    private static double power(double x, double y){
+
+        double result=1;
+        int i=1;
+        while(i <= y)
+        {
+            result *= x;
+            i++;
+        }
+        return result;
+    }
 }
